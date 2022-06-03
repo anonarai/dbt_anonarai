9e59ba7a5acb49d66895009d05ccf70b93b2400f
anona@communityconnectlabs.com <anona@communityconnectlabs.com>
2022-06-03 15:29:39 +0000

    updated contact and messages reporting tables, from airbyte ccl

--- models/messages/msgs_contact_info.sql
+++ models/messages/msgs_contact_info.sql
@@ -15,8 +15,6 @@ msg_urns as (
     select 
         msgs.id as msg_id,
         msgs.text as message,
-        msgs.contact_urn_id as contact_urn,
-        msgs.contact_id as contact_id,
         msgs.org_id,
         msgs.sent_on,
         msgs.error_count,
@@ -25,7 +23,9 @@ msg_urns as (
         msgs.channel_id,
         msgs.next_attempt,
         msgs.response_to_id,
-        msgs.uuid,
+        msgs.uuid as msg_uuid,
+        msgs.contact_urn_id as contact_urn,
+        msgs.contact_id as contact_id,
         urns.contact,
         urns.org_name,
         urns.channel
@@ -36,7 +36,22 @@ msg_urns as (
 
 msgs_full_contacts as (
     select 
-        *,
+        msg_urns.msg_id,
+        msg_urns.msg_uuid,
+        msg_urns.message,
+        msg_urns.org_id,
+        msg_urns.sent_on,
+        msg_urns.error_count,
+        msg_urns.direction,
+        msg_urns.msg_count,
+        msg_urns.channel_id,
+        msg_urns.next_attempt,
+        msg_urns.response_to_id,
+        msg_urns.contact_urn,
+        msg_urns.contact_id,
+        msg_urns.contact,
+        msg_urns.org_name,
+        msg_urns.channel,
         contacts.contact_name,
         contacts.created_on as contact_created_date,
         contacts.last_seen_on as contact_last_seen,
@@ -48,4 +63,3 @@ msgs_full_contacts as (
     left join contacts on msg_urns.contact_id = contacts.contact_id
 )
 select * from msgs_full_contacts
-
