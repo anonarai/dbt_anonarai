aeb22e7e44217645a86ebef31a18128fcb167baf
anona@communityconnectlabs.com <anona@communityconnectlabs.com>
2022-07-20 20:45:19 +0000

    updated usage table

--- models/twilio_usage/twilio_usage.sql
+++ models/twilio_usage/twilio_usage.sql
@@ -1,15 +1,26 @@
 with usage as (
     select 
-        *
+        account_id,
+        description,
+        usage,
+        usage_unit,
+        count,
+        count_unit,
+        price,
+        price_unit,
+        start_date,
+        end_date
     from PC_FIVETRAN_DB.twilio_gnp.usage_record
 ),
 
 final as (
     select
-        usage.*,
-        friendly_name as account_name
+        friendly_name as account_name,
+        usage.*
     from PC_FIVETRAN_DB.twilio_gnp.account_history
+    
     full outer join usage on usage.account_id = PC_FIVETRAN_DB.twilio_gnp.account_history.id
+    where PC_FIVETRAN_DB.twilio_gnp.account_history.status like '%active%'
 )
 
 select * from final
