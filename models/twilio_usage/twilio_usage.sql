93e21e94e6c83d5491d5ee561b3b1115aecea739
anona@communityconnectlabs.com <anona@communityconnectlabs.com>
2022-07-25 19:21:28 +0000

    edits to twilio usage table

--- models/twilio_usage/twilio_usage.sql
+++ models/twilio_usage/twilio_usage.sql
@@ -13,14 +13,31 @@ with usage as (
     from PC_FIVETRAN_DB.twilio_gnp.usage_record
 ),
 
-final as (
-    select
-        friendly_name as account_name,
-        usage.*
-    from PC_FIVETRAN_DB.twilio_gnp.account_history
-    
-    full outer join usage on usage.account_id = PC_FIVETRAN_DB.twilio_gnp.account_history.id
-    where PC_FIVETRAN_DB.twilio_gnp.account_history.status like '%active%'
-)
+categories as (
+    select 
+    usage.*,
+    analytics.dbt_anonarai.twilio_categories.description as descrip 
+    from analytics.dbt_anonarai.twilio_categories
+    full outer join usage on usage.description = analytics.dbt_anonarai.twilio_categories.description
+),
 
+final as (
+    select 
+        PC_FIVETRAN_DB.twilio_gnp.account_history.friendly_name as account_name,
+        categories.account_id,
+        categories.description,
+        categories.usage,
+        categories.usage_unit,
+        categories.count,
+        categories.count_unit,
+        categories.price,
+        categories.price_unit,
+        categories.start_date,
+        categories.end_date
+    from categories
+    full outer join PC_FIVETRAN_DB.twilio_gnp.account_history on PC_FIVETRAN_DB.twilio_gnp.account_history.id =categories.account_id
+    where PC_FIVETRAN_DB.twilio_gnp.account_history.status like '%active%'
+    and descrip is not null
+)
 select * from final
+
